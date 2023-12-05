731f20bf2a61d58f8838b3f72e13c30545dc9c33
The Chief <lurbevil@myuax.com>
2023-12-05 13:14:56 +0100

    Creacion del primer contenedor para los datos de las casas

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
+import React from "react";
+import Container from "@/app/components/Container";
 
 const Navbar = () => {
     return (
-        <div className="navbar">
-            <div className="logo">
-                imagen_airbnb
-            </div>
-            <div className="search">
-                <input type="text" placeholder="Start your search" />
-                busqueda
-            </div>
-            <div className="menu">
-                <div className="menu-item">Become a host</div>
-                <div className="menu-item">Help</div>
-                <div className="menu-item">Sign up</div>
-                <div className="menu-item">Log in</div>
+        <div className="fixed w-full bg-white z-10 shadow-sm">
+            <div className="py-4 border-b-[1px]">
+                <Container>
+                </Container>
             </div>
         </div>
     )
 }
-
-
 export default Navbar;
