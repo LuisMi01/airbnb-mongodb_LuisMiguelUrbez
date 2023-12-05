c7c4dd4d38b3debfaa849ccb5b483b3d866e488e
The Chief <lurbevil@myuax.com>
2023-12-05 15:39:25 +0100

    Buscando mostar la imagen

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -7,7 +7,9 @@ import { useRouter } from 'next/navigation'
 const Logo = () => {
     const router = useRouter()
     return (
-        <Image alt="Logo" className="hidden ms:block cursor-pointer" src="/images/logo.png" width={100} height={100} onClick={() => router.push('/')} />
+        <Image alt="Logo" className="hidden ms:block cursor-pointer" src="/public/images/logo.png" width="100" height="100" onClick={() =>
+            router.push('https://www.uax.com/')
+        } />
     )
 }
 
