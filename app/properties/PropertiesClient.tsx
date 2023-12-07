a1ff4bf3204ec39e1e4b181a0c7713493554ecd5
The Chief <lurbevil@myuax.com>
2023-12-08 00:09:31 +0100

    controlador para la pagina de propiedades creado

--- app/properties/PropertiesClient.tsx
+++ app/properties/PropertiesClient.tsx
@@ -46,17 +46,7 @@ const PropertiesClient: React.FC<PropertiesClientProps> = ({
                 title="Propiedades"
                 subtitle={`Lista de las propiedades de ${currentUser?.name}`}
             />
-            <div
-                className="mt-10 grid
-          grid-cols-1
-          sm:grid-cols-2
-          md:grid-cols-3
-          lg:grid-cols-4
-          xl:grid-cols-5
-          2xl:grid-cols-6
-          gap-8
-        "
-            >
+            <div className="mt-10 grid  grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
                 {listings.map((listing: any) => (
                     <ListingCard
                         key={listing.id}
