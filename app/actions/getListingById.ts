f95b6013bf707ef4566f27c197bec7b1d6638b3f
The Chief <lurbevil@myuax.com>
2023-12-07 23:09:52 +0100

    solucionado el error que no debaja visualizar casas creadas por usuarios q no estaban registrados en la base de datos

--- app/actions/getListingById.ts
+++ app/actions/getListingById.ts
@@ -1,4 +1,5 @@
 import prisma from "@/app/libs/prismadb";
+import {toast} from "react-hot-toast";
 
 interface IParams {
     listingId?: string;
@@ -14,24 +15,42 @@ export default async function getListingById(
             where: {
                 id: listingId,
             },
-            include: {
-                user: true
-            }
         });
 
         if (!listing) {
             return null;
         }
 
+        // Try to get the user
+        let user = await prisma.user.findUnique({
+            where: {
+                id: listing.userId,
+            },
+        });
+
+        // If the user doesn't exist, create a dummy user
+        if (!user) {
+            user = {
+                id: 'unknown',
+                name: null,
+                email: null,
+                emailVerified: null,
+                image: null,
+                hashedPassword: null,
+                favoriteIds: [],
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            };
+        }
+
         return {
             ...listing,
             createdAt: new Date(listing.createdAt),
             user: {
-                ...listing.user,
-                createdAt: new Date(listing.user.createdAt),
-                updatedAt: new Date(listing.user.updatedAt),
-                emailVerified:
-                    listing.user.emailVerified?.toString() || null,
+                ...user,
+                createdAt: new Date(user.createdAt),
+                updatedAt: new Date(user.updatedAt),
+                emailVerified: user.emailVerified?.toString() || null,
             }
         };
     } catch (error: any) {
