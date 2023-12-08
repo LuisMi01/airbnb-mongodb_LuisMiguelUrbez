2787dff3842b982b1de5a11264ccee74f95f128f
The Chief <lurbevil@myuax.com>
2023-12-08 11:24:59 +0100

    los filtros funionan aunque las querys para las fechas no estan implementadas aun

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,20 +2,64 @@ import prisma from "@/app/libs/prismadb";
 
 export interface IListingParams {
     userId?: string
+    guests?: number
+    roomCount?: number
+    bathroomCount?: number
+    startDate?: string
+    endDate?: string
+    locationValue?: string
+    category?: string
+
 }
 
 export default async function getListings(
    params: IListingParams
+
 ) {
 
     try{
-        const {userId} = params
+        const {
+            userId,
+            roomCount,
+            bathroomCount,
+            startDate,
+            endDate,
+            locationValue,
+            category, guests
+        } = params
 
         let query:any = {}
 
         if(userId) {
             query.userId = userId
         }
+        if(category){
+            query.category = category
+        }
+
+        if(roomCount){
+            query.roomCount = {
+                gte: +roomCount  //el + es necesario para convertir el string a number
+            }
+        }
+
+        if(guests){
+            query.guests = {
+                gte: +guests
+            }
+        }
+
+        if(bathroomCount){
+            query.bathroomCount = {
+                gte: +bathroomCount
+            }
+        }
+
+        if (locationValue){
+            query.locationValue = locationValue
+        }
+
+
         const listings = await prisma.listing.findMany({
             where: query,
             orderBy: {
